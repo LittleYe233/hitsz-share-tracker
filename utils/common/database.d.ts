4b02158be886fc29d7c80cb42554c76b50168b9d
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-03-15 12:24:05 +0800

    Update database utility as an archive
    It's a pity that the database utility needs a little big update on its
    supported databases. We found that Redis is very lite but doesn't
    support some features of an object-relational database which may have
    a boost on our development. So we decided to archive all the supports
    on Redis and replace it with MySQL. Actually, MySQL can "define" a
    transaction that can easily roll back if some steps fail. This feature
    makes the "message queues" (in the future?) more convenient.

--- utils/common/database.d.ts
+++ utils/common/database.d.ts
@@ -10,18 +10,18 @@
 
 import * as mysql from 'mysql';
 import { RedisClientType as _RedisClientType } from '@node-redis/client';
-import { BasicConnectionConfig } from './config';
+import { BasicConnectionConfig, BasicRedisConfig } from './config';
 
-type DatabaseConn = BasicConnectionConfig & {
+type _DatabaseConn = BasicConnectionConfig & {
   // important for internal use
   conn?: unknown;  // connection
 
   // methods
-  connect?: CallableFunction;
-  disconnect?: CallableFunction;
+  connect?(): unknown;
+  disconnect?(): unknown;
 }
 
-export type _MySQLConn = DatabaseConn & {
+export type _MySQLConn = _DatabaseConn & {
   // from arguments
   db?: string;
   tbl?: string;
@@ -30,10 +30,10 @@ export type _MySQLConn = DatabaseConn & {
   conn?: mysql.Connection;
 
   // methods
-  query?: CallableFunction;
+  query?: mysql.QueryFunction;
 }
 
-export type _RedisConn = DatabaseConn & {
+export type _RedisConn = _DatabaseConn & {
   // from arguments
   db?: number;
   key?: string;
@@ -44,3 +44,16 @@ export type _RedisConn = DatabaseConn & {
   // methods
   // NOTE: For Redis built-in commands, use `RedisConn.conn` instead.
 }
+
+type _ProjectDatabaseConn = {
+  initialize?(): Promise<number>;
+}
+
+type _ActiveClientsSpecConfig = {
+  max_clients?: number;
+}
+
+export type _ActiveClientsConfig = BasicRedisConfig & _ActiveClientsSpecConfig;
+
+export type _AuthUsersConn = _MySQLConn & _ProjectDatabaseConn;
+export type _ActiveClientsConn = _RedisConn & _ProjectDatabaseConn & _ActiveClientsSpecConfig;
--- utils/common/database.js
+++ utils/common/database.js
@@ -13,6 +13,8 @@
 const mysql = require('mysql');
 const redis = require('redis');
 
+const MAX_CLIENTS = 1000;  // It's a placeholder, and may be removed later.
+
 /**
  * @param {import('./config').BasicMySQLConfig} params
  */
@@ -84,6 +86,30 @@ function RedisConn(params={}) {
     inst.conn = redis.createClient({ url: url });
     
     return inst.conn.connect();
+  };
+
+  return inst;
+}
+
+/**
+ * @param {import('./database')._ActiveClientsConfig} params
+ */
+// @ts-ignore
+function _ActiveClientsConn(params={}) {
+  /** @type {import('./database')._ActiveClientsConn} */
+  let inst = {
+    ...RedisConn(params),
+    max_clients: (params.max_clients == 0) ? MAX_CLIENTS : params.max_clients
+  };
+
+  /**
+   * Initialize the database.
+   * @note This is a destructive operation that will erase all previous data and
+   * reset the database to a default state.
+   * @returns a Promise storing the returning value of the last command
+   */
+  inst.initialize = async () => {
+    return await inst.conn.del(inst.key);
   };
 
   return inst;
