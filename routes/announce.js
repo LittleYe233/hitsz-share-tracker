7739e037c091b38d3377251ba32b98c6d7f7fd6e
LittleYe233 <30514318+LittleYe233@users.noreply.github.com>
2022-03-31 14:45:22 +0800

    Use asynchronous methods for Express routers

--- routes/announce.js
+++ routes/announce.js
@@ -1,9 +1,8 @@
-var express = require('express');
 const url = require('url');
 const { validate } = require('../utils/announce/process');
-var router = express.Router();
+const router = require('express-promise-router')();
 
-router.get('/announce', function(req, res, next) {
+router.get('/announce', async function(req, res, next) {
   // process
   let params = url.parse(req.url, true).query;
   params.ip = params.ip || req.ip;  // ip must exist
@@ -11,11 +10,14 @@ router.get('/announce', function(req, res, next) {
   if (params.ip.substring(0, 7) === '::ffff:') {
     params.ip = params.ip.substring(7);
   }
-  const validation = validate(params);
+  // validate
+  const validated = validate(params);
+  // communicate with databases
+  // TODO
 
   // send responses
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(validation.result);
+  res.end(validated.result);
 });
 
 router.get('/_test_announce', function(req, res, next) {
